8c49d70708f6020dc6c26c8628e45a5fb3b29bb8
Mikhail Konchyts <mixamtk@gmail.com>
2023-05-23 13:54:11 +0300

    solution

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -4,6 +4,11 @@ import static org.apache.commons.lang3.StringUtils.*;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        return isNumeric(str) && Double.parseDouble(str) > 0;
+        try {
+            double number = Double.parseDouble(str);
+            return number > 0;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
