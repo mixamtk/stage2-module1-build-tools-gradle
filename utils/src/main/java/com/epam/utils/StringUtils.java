b65392d475c030d04239a2eb3598410639b59666
Mikhail Konchyts <mixamtk@gmail.com>
2023-05-23 15:58:09 +0300

    refactoring

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -4,6 +4,6 @@ import static org.apache.commons.lang3.StringUtils.*;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        return (str != null) && str.matches("[1-9]\\d+\\.?\\d+") && Double.parseDouble(str) > 0;
+        return (str != null) && str.matches("([1-9]\\d*)(\\.)?(\\d*)") && Double.parseDouble(str) > 0;
     }
 }
